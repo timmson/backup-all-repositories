494b3da20a3fb2a92f050892a45f7ea0ad2a2b1c
timmson <timmson666@mail.ru>
2024-01-08 22:20:04 +0300

    Remove github-api

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const fs = require("fs")
 const https = require("https")
-const GitHub = require("github-api")
+const axios = require("axios")
 
 let login = "timmson" //user
 
+const listRepos = async (user) => {
+    const result = await axios.get(`https://api.github.com/users/${user}/repos?per_page=100`)
+    return result.data
+}
+
 const download = (url, filename) => {
-    console.log(`${url} is downloading`);
+    console.log(`${url} => ${filename}`)
     https.get(url, (res) => {
-        const path = `${__dirname}/repos/${filename}`;
-        const filePath = fs.createWriteStream(path);
-        res.pipe(filePath);
+        const path = `${__dirname}/repos/${filename}`
+        const filePath = fs.createWriteStream(path)
+        res.pipe(filePath)
         filePath.on("finish", () => {
-            filePath.close();
-            console.log(`${filename} is downloaded`);
+            filePath.close()
+            console.log(`${filename} is OK`)
         })
     })
 }
 
-const user = new GitHub().getUser(login)
-user.listRepos(function (err, repos) {
-    console.log(`Repos: ${repos.length}`)
-    repos.filter((it) => it.owner.login === login)
-        .map((it) => ({url: `https://codeload.github.com/${it.owner.login}/${it.name}/zip/refs/heads/${it.default_branch}`, filename: `${it.name}.zip`}))
-        .forEach((it) => download(it.url, it.filename))
+listRepos(login).then((repos) => {
+    const downloads = repos.filter((it) => it.owner.login === login && !it.archived)
+        .map((it) => (
+                {
+                    url: `https://codeload.github.com/${it.owner.login}/${it.name}/zip/refs/heads/${it.default_branch}`,
+                    filename: `${it.name}.zip`
+                }
+            )
+        )
+    downloads.forEach((it) => download(it.url, it.filename))
 })
